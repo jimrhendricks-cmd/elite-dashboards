0bb4d84358de241b7044292f091e1830114bc61f
Jim Hendricks <jim.r.hendricks@gmail.com>
2026-04-01 22:31:37 -0600

    Add PWA app mode + mobile optimization

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'elite-hq-v1';
+const ASSETS = [
+  '/elite-dashboards/',
+  '/elite-dashboards/index.html',
+  '/elite-dashboards/manifest.json',
+  '/elite-dashboards/icon-192.png',
+  '/elite-dashboards/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(resp => {
+        const clone = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return resp;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
